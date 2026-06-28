8790acaa0dae9f67031876ebf8cf5a12d3e8d0f8
Subhajit Maity <98680427+Subhajit2007@users.noreply.github.com>
2026-06-28 22:35:06 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(reg => {
+        console.log("Service Worker registered!", reg);
+    });
+}
+
+document.getElementById('notifyBtn').addEventListener('click', () => {
+    Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+            // Service Worker er maddhome notification pathano
+            navigator.serviceWorker.ready.then(reg => {
+                reg.showNotification("Hello Subhajit!", {
+                    body: "Service Worker diye notification elo!",
+                    icon: "https://cdn-icons-png.flaticon.com/512/3233/3233519.png"
+                });
+            });
+        }
+    });
+});
 
